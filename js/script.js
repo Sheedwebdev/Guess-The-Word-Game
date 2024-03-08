c864587982446c260f20ebd010e1dee239499e63
Rashid Enahora <120060745+Sheedwebdev@users.noreply.github.com>
2024-03-07 19:09:45 -0500

    Displaying word and guesses

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Part1: Create global variables for selecting all targeted elements on the page
+// Part1: Create global variables (include varibles for selecting all targeted elements on the page)
 const guess = document.querySelector(".guessed-letters"); // step1: Declare a variable to target the unordered list where the players guess will appear
 const guessButton = document.querySelector(".guess"); // step2: Declare a variable to target the guess button
 const input = document.querySelector(".letter"); // step3: Declare a variable to target the input element
@@ -15,7 +15,7 @@ const guessedLetters = [];
 const update = function (word) { // step9: Create a function expression using the word to guessed as the parameter
   const updatedLetters = []; // step10: Declare a variable and assign an empty array to it
   for (const letter of word) { // step11: Loop through each letter in the array word 
-    console.log(letter);  // step12: Check whether each element (letter) was loop through 
+    console.log(letter);  // step12: Check whether each element (letter) was looped through 
     updatedLetters.push("●"); // step13: Push a "●" into the empty array after each loop/iteration (Since there are 8 letters in magnolio, there should be 8 "●")
   }
   progress.innerText = updatedLetters.join(""); // step14: Set the inner text of the word in progress paragraph to array of "●" all  joined together like a string
@@ -29,17 +29,17 @@ guessButton.addEventListener("click", function (e)  { // step16: Apply an event 
   responseMessage.innerText = ""; //Clear the previous response message everytime the player clicks the guess button
   const capturedInput = input.value; // step18: Declare a variable to capture the input value
   // console.log(capturedInput); // step19: Check to see if the input value is being captured
-  const goodGuess = inputValidation(capturedInput);
-  if (goodGuess) {
-    makeGuess(capturedInput);
+  const goodGuess = inputValidation(capturedInput); // step23: Declare a variable that make sure the player's input is valid by calling the inputValidation funciton from part4 
+  if (goodGuess) { // step28: Write a conditonal block that determines what to do if input is valid
+    makeGuess(capturedInput); // step29: If input is valid, than call the makeGuess function from part5 to make sure the player does not guess the same value more than once
   }
-  input.value = ""; // step20: Clear the value each time its been captured
+  input.value = ""; // step30: Clear the value each time its been captured
 });
 
 // Part4: Create a function to check the player's input
-const inputValidation = function (input) {
-  const acceptedLetter = /[a-zA-Z]/;
-  if (input.length === 0) {
+const inputValidation = function (input) { // step20: Define a funtion expression that validates the captured input value
+  const acceptedLetter = /[a-zA-Z]/; // step21: Declare a variable that defines the range of accepted letters
+  if (input.length === 0) { // step22: Write a conditional block for all three invalid player input values 
     responseMessage.innerText = "Please enter a letter.";
   } else if (input.length > 1) {
     responseMessage.innerText = "Please enter a single letter.";
@@ -50,16 +50,51 @@ const inputValidation = function (input) {
   }
 };
 
-// Part5: Create a function to capture input
-const makeGuess = function (capturedInput) {
-  capturedInput = capturedInput.toUpperCase();
-  if (guessedLetters.includes(capturedInput)) {
+// Part5: Create a function that captures a valid input value and displays a message if that value has already been captured
+const makeGuess = function (capturedInput) { // step24: Define a function expression 
+  capturedInput = capturedInput.toUpperCase();  // step25: Change all valid input values to upper case 
+  if (guessedLetters.includes(capturedInput)) {  // step26: Write a condition block visa vis whether the array already includes the captured value 
     responseMessage.innerText = "You already guessed that letter, silly. Try again."
   } else {
     guessedLetters.push(capturedInput);
-    console.log(guessedLetters);
+    // console.log(guessedLetters);  // step27: When this funciton is ran, check whether valid inputs are pushed to guessedLetters array
+    displayedLetters();
+    updatedWord(guessedLetters);
   }
 };
 
-  
-  
+// Part6: Create a function that displays the guessed letters
+const displayedLetters = function () {
+  guess.innerHTML = "";
+  for (const letter of guessedLetters) {
+    const li = document.createElement("li");
+    li.innerText = letter;
+    guess.append(li);
+  }
+};
+
+// Part7: Create a function that updates the word in progress
+const updatedWord = function (guessedLetters) {
+  const wordUpper = word.toUpperCase();
+  const wordArray = wordUpper.split("");
+  const displayWord = [];
+  for (const letter of wordArray) {
+    if (guessedLetters.includes(letter)) {
+      displayWord.push(letter.toUpperCase());
+    } else {
+      displayWord.push("●");
+    }
+  }
+  // console.log(displayWord);
+  progress.innerText = displayWord.join("");
+  won();
+};
+
+// Part8: Create a function that determines if the player has won!
+const won = function () {
+  if (word.toUpperCase() === progress.innerText) {
+    responseMessage.classList.add("win");
+    responseMessage.innerHTML = `<p class="highlight">You guessed the correct word! Congrats!</p`;
+  }
+};
+
