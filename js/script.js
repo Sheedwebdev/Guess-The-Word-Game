3f62888f8ffb80af20b0807540af7e76a70b788e
Rashid Enahora <120060745+Sheedwebdev@users.noreply.github.com>
2024-07-12 19:52:18 -0400

    completed part4 of 7th rep

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 //Reps Completed: 6
 
 //Part1: Global Variables
-
+const responseMessage = document.querySelector(".message");
+const progress = document.querySelector(".word-in-progress");
+const remaining = document.querySelector(".remaining");
+const remainingSpan = document.querySelector(".remaining span");
+const guess = document.querySelector(".guessed-letters");
+const input = document.querySelector(".letter");
+const guessButton = document.querySelector(".guess");
+const playAgainButton = document.querySelector(".play-again");
+let secret = "viola";
+let storedGuesses = [];
+let countRemaining = 8;
 
 
 // Part10: Randomly pick a word from a list of 823 words 
@@ -9,16 +19,39 @@
 
 
 // Part2: Add placeholders for each letter in the word
-
+const placeholder = function (secret) {
+	const placeholdersArray = [];
+	for (const letter of secret) {
+		placeholdersArray.push("?");
+	}
+	progress.innerText = placeholdersArray.join("");
+};
+placeholder(secret);
 
 
 // Part3: Capture the input when the guess button is clicked
-
+guessButton.addEventListener("click", function (e) {
+	e.preventDefault();
+	const capturedValue = input.value;
+	//Extention of Part4
+	const goodGuess = validGuess(capturedValue);
+	input.value = "";
+});
 
 
 // Part4: Validate the player's guess
-
-
+const validGuess = function (capturedValue) {
+	const onlyLetters = /[a-zA-Z]/;
+	if (capturedValue.length === 0) {
+		responseMessage.innerText = "Please make a guess first!";
+	} else if (capturedValue.length > 1) {
+		responseMessage.innerText = "Please enter only one guess at a time!";
+	} else if (!capturedValue.match(onlyLetters)) {
+		responseMessage.innerText = "Please enter only letters from A to Z!";
+	} else {
+		return capturedValue;
+	}
+};
 
 
 // Part5: What happens when the player makes a valid guess?
